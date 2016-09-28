bae8e6c12f3eb7ec47d825966fb6bea196ca4d48
mdarafathossain <arafat.ruet03@gmail.com>
2016-09-28 18:31:35 +1000

    ipv6 specific offset or constants which is required to parse ipv6 pcap file

---
+++ src/edu/gatech/sjpcap/PcapParserv6.java
@@ -0,0 +1,14 @@
+package edu.gatech.sjpcap;
+
+
+	public class PcapParserv6 
+	{
+	    public static final int ipProtoOffset = 10;
+	    public static final int ipSrcOffset = 12;
+	    public static final int ipDstOffset = 28;
+	    public static final int etherHeaderLength = 4;
+	    public static final int etherTypeOffset = 0;
+	    public static final int etherTypeIP = 0x1e00;
+	}
+
+
